325cbbd92757a3e75f500bbbd624114174832e81
sudo <genki.genki.1117@gmail.com>
2021-11-20 00:15:17 +0900

    マイグレーション編集中

---
+++ database/migrations/2021_11_19_150619_create_shops_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->id('shop_id');
+            $table->string('shop_name');
+            $table->unsignedBigInteger('area_id');
+            $table->unsignedBigInteger('jenre_id');
+            $table->forgen('area_id')->references('area_id')->on('areas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shops');
+    }
+}

6bd03b227f26212cc332e7bafd3f91b70825e7cb
sudo <genki.genki.1117@gmail.com>
2021-12-25 23:54:45 +0900

    area

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(ShopsTableSeeder::class);
+        $this->call(AreasTableSeeder::class);
     }
 }
